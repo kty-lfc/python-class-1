1453a1faebea20f1a5d2e77d4936c3d5f9d7d56f
kty-lfc <58500595+kty-lfc@users.noreply.github.com>
2021-06-18 12:27:03 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,29 @@
+# print("Hello", "how are you?")
+'''
+a = 9
+b = 10
+# print(a, "+", b)
+if a > b:
+    print("a more than b")
+elif a == b:
+    print("a equal b")
+else:
+    print("a less than b")
+
+c = a+b
+print("c=", c)
+'''
+
+txt = "The best things in life are LOVE!",
+if "love" in txt:
+    print("Yes, 'love' is present.")
+else:
+    print("Not found that word!!")
+
+'''
+price = "49"
+txt = "The price is {} dollars"
+print(txt.format(price))
+print(txt.format(c))
+print("The price is " + price + " dollars")
+'''
---
+++ helloworld.py
@@ -0,0 +1,7 @@
+'''
+print("Hello World!!")
+a = 10
+print(a)
+'''
+username = input("Enter username:")
+#print("Username is: " + username)
